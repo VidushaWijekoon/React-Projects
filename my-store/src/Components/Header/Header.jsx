ebf7eb16ed09b072aa9b06cdd6d1c3033f8b2595
VidushaWijekoon <vidusha.wijekoon11@gmail.com>
2023-10-01 17:37:39 +0400

    header

--- my-store/src/Components/Header/Header.jsx
+++ my-store/src/Components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="">
+        <img />
+        <h1>
+          Vidusha <span>My Store</span>
+        </h1>
+        <div className="">
+          <input type="text" placeholder="Search" />
+          <img />
+        </div>
+      </div>
+      <img />
+    </header>
+  );
 };
 
 export default Header;
