b619513236581fe1aab6551307e09f5ec6c0d70c
VidushaWijekoon <vidusha.wijekoon11@gmail.com>
2023-10-03 18:20:51 +0400

    basics

--- 1.basics/fundamentals/src/Components/Main.jsx
+++ 1.basics/fundamentals/src/Components/Main.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-const Main = () => {
-  return <div>Main</div>;
+// const name = "Vidusha Wijekoon";
+// const position = "Web Developer";
+
+// const myObj = {
+//   name: `${name}`,
+//   position: `${position}`,
+//   city: "Al Sharjah",
+// };
+
+const Main = ({ children, name, city, position, imgUrl }) => {
+  // Object Destructor
+  // const { city } = myObj;
+  return (
+    <div>
+      <img
+        src={
+          imgUrl
+            ? `${imgUrl}`
+            : `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyMpDwUGXCA3ZSHLdGaS9bsJAhuvQWgYhIsw&usqp=CAU`
+        }
+        alt="cardImage"
+      />
+      <div className="mainBlock__details">
+        <h3>{!name ? "no name" : `${name}`}</h3>
+        <p>
+          <span>{!city ? "no city" : `${city}`}</span>
+          <span>{!position ? "no position" : `${position}`}</span>
+        </p>
+      </div>
+      {children}
+    </div>
+  );
 };
 
 export default Main;
---
+++ 1.basics/fundamentals/src/Data/myData.jsx
@@ -0,0 +1,77 @@
+const myData = [
+  {
+    id: 1,
+    name: "Amila",
+    city: "Tangalle",
+    position: "Web Developer",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi7h6S7xrNzc6ihoCi5p-mdDfoRJncOAkeNA&usqp=CAU",
+  },
+  {
+    id: 2,
+    name: "Vidusha",
+    city: "Kandy",
+    position: "Software Developer",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi7h6S7xrNzc6ihoCi5p-mdDfoRJncOAkeNA&usqp=CAU",
+  },
+  {
+    id: 3,
+    name: "John",
+    city: "Dubai",
+    position: "Dev Ops Developer",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi7h6S7xrNzc6ihoCi5p-mdDfoRJncOAkeNA&usqp=CAU",
+  },
+  {
+    id: 4,
+    name: "Upul",
+    city: "Chengdu",
+    position: "QA Engineer",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi7h6S7xrNzc6ihoCi5p-mdDfoRJncOAkeNA&usqp=CAU",
+  },
+  {
+    id: 5,
+    name: "Kumara",
+    city: "Colombo",
+    position: "App Developer",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi7h6S7xrNzc6ihoCi5p-mdDfoRJncOAkeNA&usqp=CAU",
+  },
+];
+
+export default myData;
+
+export const myData2 = [
+  {
+    id: 1,
+    name: "Amila",
+    city: "Tangalle",
+    position: "Web Developer",
+  },
+  {
+    id: 2,
+    name: "Vidusha",
+    city: "Kandy",
+    position: "Software Developer",
+  },
+  {
+    id: 3,
+    name: "John",
+    city: "Dubai",
+    position: "Dev Ops Developer",
+  },
+  {
+    id: 4,
+    name: "Upul",
+    city: "Chengdu",
+    position: "QA Engineer",
+  },
+  {
+    id: 5,
+    name: "Kumara",
+    city: "Colombo",
+    position: "App Developer",
+  },
+];
