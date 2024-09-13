663fb162c1937428a48ee124b5164a6e7b8e499e
vidusha wijekoon <vidusha.wijekoon11@gmail.com>
2024-09-13 06:46:40 +0400

    context

--- survery-interface/src/Components/SurveysListItem.jsx
+++ survery-interface/src/Components/SurveysListItem.jsx
@@ -3,36 +3,35 @@ import {
     PencilIcon,
     TrashIcon,
 } from "@heroicons/react/24/outline";
-import React from "react";
-import TButton from "./core/TButton";
+import TButton from "./Core/TButton";
 
-export default function SurveyListItem({ survey, onDeleteClick }) {
+export default function SurveyListItem({ survery, onDeleteClick }) {
     return (
         <div className="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]">
             <img
-                src={survey.image_url}
-                alt={survey.title}
+                src={survery.image_url}
+                alt={survery.title}
                 className="w-full h-48 object-cover"
             />
-            <h4 className="mt-4 text-lg font-bold">{survey.title}</h4>
+            <h4 className="mt-4 text-lg font-bold">{survery.title}</h4>
             <div
-                dangerouslySetInnerHTML={{ __html: survey.description }}
+                dangerouslySetInnerHTML={{ __html: survery.description }}
                 className="overflow-hidden flex-1"
             ></div>
 
             <div className="flex justify-between items-center mt-3">
-                <TButton to={`/surveys/${survey.id}`}>
+                <TButton to={`/surverys/${survery.id}`}>
                     <PencilIcon className="w-5 h-5 mr-2 " />
                     Edit
                 </TButton>
                 <div className="flex items-center">
-                    <TButton href={`/view/survey/${survey.slug}`} circle link>
+                    <TButton href={`/view/survery/${survery.slug}`} circle link>
                         <ArrowTopRightOnSquareIcon className="w-5 h-5" />
                     </TButton>
 
-                    {survey.id && (
+                    {survery.id && (
                         <TButton
-                            onClick={(ev) => onDeleteClick(survey.id)}
+                            onClick={(ev) => onDeleteClick(survery.id)}
                             circle
                             link
                             color="red"
